a6450a40075da131a3c51f09a8556f9e78291ea5
Arsh Momin <arsh.momin2k@gmail.com>
2023-02-22 20:54:08 +0500

    imdb clone

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavbarItems from './NavbarItems'
+
+function Navbar() {
+  return (
+    <div className='flex justify-center dark:bg-gray-600 bg-amber-100 lg:text-lg p-4'>
+        <NavbarItems title="Trending" param="fetchTrending" />
+        <NavbarItems title="Top Rated" param="fetchTopRated" />
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/app/loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function loading() {
+  return (
+    <div className='flex justify-center items-center'>
+        <h1>loading...</h1>
+        {/* <img className='h-96 ' src="rolling.svg" alt="loading..." /> */}
+    </div>
+  )
+}
+
+export default loading
